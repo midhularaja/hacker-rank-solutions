5af4c5f7f8c345051508dd957de50cfe156356ec
midhularaja <167186044+midhularaja@users.noreply.github.com>
2025-06-23 15:35:41 +0530

    Add files via upload

---
+++ Simple_Array_Sum.py
@@ -0,0 +1,38 @@
+#Problem: Simple Array Sum
+#Link: https://www.hackerrank.com/challenges/simple-array-sum/problem
+#Difficulty: Easy
+ 
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'simpleArraySum' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY ar as parameter.
+#
+
+def simpleArraySum(ar):
+    # Write your code here
+    ans=0
+    for i in ar:
+        ans+=i
+    return ans
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = simpleArraySum(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
