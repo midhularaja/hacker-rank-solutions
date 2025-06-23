26de48ecb2b902068cc47bb18fc6cda3bea85f73
midhularaja <167186044+midhularaja@users.noreply.github.com>
2025-06-23 14:55:38 +0530

    Add files via upload

---
+++ Mini_Max_Sum.py
@@ -0,0 +1,31 @@
+#Problem: Mini-Max Sum
+#Link: https://www.hackerrank.com/challenges/staircase/problem
+#Difficulty: Easy
+ 
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'staircase' function below.
+#
+# The function accepts an INTEGER n as a parameter.
+#
+
+def staircase(n):
+    # Write your code here
+    for i in range(n):
+        for k in range(n-i-1):
+           print ("",end=" ")
+        for j in range (i+1):
+            print("#",end="") 
+        print ()
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    staircase(n)
