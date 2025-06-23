57e6ccb383b237a4437449835951a79fc2a527d6
midhularaja <167186044+midhularaja@users.noreply.github.com>
2025-06-23 15:41:15 +0530

    Add files via upload

---
+++ Sales_by_Match.py
@@ -0,0 +1,41 @@
+#Problem: Sales by Match
+#Link: https://www.hackerrank.com/challenges/three-month-preparation-kit-sock-merchant/problem
+#Difficulty: Easy
+ 
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'sockMerchant' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY ar
+#
+
+def sockMerchant(n, ar):
+    # Write your code here
+    pairs=0
+    for color in set(ar):
+        count=ar.count(color)
+        pairs+=count//2
+    return pairs
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
