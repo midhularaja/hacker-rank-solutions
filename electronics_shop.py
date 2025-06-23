d3bb9e1457a3821f3281a508a579cfd638ba07b5
midhularaja <167186044+midhularaja@users.noreply.github.com>
2025-06-23 22:39:55 +0530

    Add files via upload

---
+++ electronics_shop.py
@@ -0,0 +1,51 @@
+#Problem: electronics shop
+#Link: https://www.hackerrank.com/challenges/electronics-shop/problem
+#Difficulty: Easy
+ 
+
+#!/bin/python3
+
+import os
+import sys
+
+#
+# Complete the getMoneySpent function below.
+#
+def getMoneySpent(keyboards, drives, b):
+    #
+    # Write your code here.
+    #
+    ans=[]
+    for k in keyboards:
+        for d in drives:
+            total = k+d
+            if total<= b:
+                ans.append(total)
+    if ans:
+        return max(ans)
+    else:
+        return -1                
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    bnm = input().split()
+
+    b = int(bnm[0])
+
+    n = int(bnm[1])
+
+    m = int(bnm[2])
+
+    keyboards = list(map(int, input().rstrip().split()))
+
+    drives = list(map(int, input().rstrip().split()))
+
+    #
+    # The maximum amount of money she can spend on a keyboard and USB drive, or -1 if she can't purchase both items
+    #
+
+    moneySpent = getMoneySpent(keyboards, drives, b)
+
+    fptr.write(str(moneySpent) + '\n')
+
+    fptr.close()
