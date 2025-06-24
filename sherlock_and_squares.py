ca1775223701062f23ea6bae62fae978960fe2dc
midhularaja <167186044+midhularaja@users.noreply.github.com>
2025-06-24 23:16:34 +0530

    Add files via upload

---
+++ sherlock_and_squares.py
@@ -0,0 +1,48 @@
+#Problem: sherlock and squares
+#Link: https://www.hackerrank.com/challenges/sherlock-and-squares/problem
+#Difficulty: Easy
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'squares' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER a
+#  2. INTEGER b
+#
+
+def squares(a, b):
+    # Write your code here
+    count=0
+    for i in range(1,b+1):
+        square =i*i
+        if square>b:
+            break
+        elif square>=a:
+            count +=1
+    return count            
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input().strip())
+
+    for q_itr in range(q):
+        first_multiple_input = input().rstrip().split()
+
+        a = int(first_multiple_input[0])
+
+        b = int(first_multiple_input[1])
+
+        result = squares(a, b)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
